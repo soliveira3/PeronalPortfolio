c3bbf08df509aa962ee3e18ea4922517cdcc6549
soliveira3 <139153543+soliveira3@users.noreply.github.com>
2025-10-31 01:04:43 -0400

    added dom and fixed cow-basic link

--- frontend/src/pages/CowBasic.jsx
+++ frontend/src/pages/CowBasic.jsx
@@ -24,7 +24,7 @@ function CowBasic() {
         setResult(null);
 
         try {
-            const response = await fetch('http://localhost:5000/api/compile', {
+            const response = await fetch('/api/compile', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ code }),
